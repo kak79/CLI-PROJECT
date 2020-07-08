08fc1681ceca79e496cce5bbc1e0e05c4a338848
kak79 <kohelka@gmail.com>
2020-07-08 16:01:29 -0500

    fixed if statements

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,7 @@ class API
  
   response = HTTParty.get "https://api.pexels.com/v1/curated?per_page=20",{headers:{"Authorization":"Bearer #{ENV['API_KEY']}"}}
   
-    response["photos"].each do |photographer_hash|
+    photos_array = response["photos"].each do |photographer_hash|
       
       Photographer.new(photographer_hash["photographer"],photographer_hash["photographer_url"],photographer_hash["src"]["original"]
       )
