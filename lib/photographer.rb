71a73e71202bc00d739a3ff768266f28280ab5da
kak79 <kohelka@gmail.com>
2020-07-09 10:44:43 -0500

    fixed the outputs

--- lib/photographer.rb
+++ lib/photographer.rb
@@ -4,36 +4,56 @@ class Photographer
     
     @@all = []
 
-    def initialize(pic_taker,pic_taker_url,orig_url)
-        @pic_taker = pic_taker
-        @pic_taker_url = pic_taker_url
-        @orig_url = orig_url
+    def initialize(attr_hash)
+        @pic_taker = attr_hash[:pic_taker]
+        @pic_taker_url = attr_hash[:pic_taker_url]
+        @orig_url = attr_hash[:orig_url]
+        save
+    end
+
+    def save
         @@all << self
     end
 
     def self.all
         @@all
     end
-  
+
     def self.pic_taker_array
         @@all.map.with_index(1) do |id,i|
             id.pic_taker
             puts "#{i}. #{id.pic_taker}"
-            break if i > 49 
+            break if i > 19 
+        end
+    end
+    
+    def self.search_pic_taker_array
+        pic_taker_array
+        numb=gets.strip.to_i - 1
+        i=0
+        @@all.select do |numb|
+            i += 1
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            puts  "#{numb.pic_taker}"
+            puts  "#{numb.pic_taker_url}"
+            puts  "#{numb.orig_url}"
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"            
+            break if i > 0
         end
     end
 
-    def self.search_pic_taker_array
-        pic_taker_array
-        j=gets.strip.to_i
-        j-=1
-        pp @@all[j]
-        
-    end
+    def display
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        puts  "#{pic_taker}"
+        puts  "#{pic_taker_url}"
+        puts  "#{orig_url}"
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    end    
 
     def self.sample_pic_taker
-        pp @@all.sample
+        pic = @@all.sample
+        pic.display
     end
-    
+
 end
 
