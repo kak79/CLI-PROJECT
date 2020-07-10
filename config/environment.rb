e78c2d1d490d90178e601b45152a787eb4e4d08d
kak79 <kohelka@gmail.com>
2020-07-10 06:35:37 -0500

    set up gemfile and require bundler

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,6 @@ require 'dotenv/load'
 require "bundler/setup"
 Bundler.require
 
-require 'httparty'
-require 'json'
-require 'pry'
-
-
-
 require_relative '../lib/cli.rb'
 require_relative '../lib/api.rb'
 require_relative '../lib/photographer.rb'
