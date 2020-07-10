2bedf74b7242aed3636a5a344378a452dfdbf405
kak79 <kohelka@gmail.com>
2020-07-10 12:51:14 -0500

    got my gemfile and bundler to work

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,6 @@
-
 require 'dotenv/load'
-
-# require "bundler/setup"
-# Bundler.require
-
-require 'httparty'
-require 'json'
-require 'pry'
-
-
+require "bundler/setup"
+Bundler.require
 
 require_relative '../lib/cli.rb'
 require_relative '../lib/api.rb'
